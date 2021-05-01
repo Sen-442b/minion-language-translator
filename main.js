ebb3107f1e1a30430eb5d5b1c0fa608b8cf60add
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-05-01 16:31:41 +0530

    textarea added

--- main.js
+++ main.js
@@ -10,5 +10,13 @@ alert(welcome_message);
 
 var button_translate = document.querySelector("#btn-translate");
 button_translate.addEventListener("click" , function clickEventHandler(){
-    console.log("clicked");
+    console.log("clicked")
 });
+
+  // exercise question 
+
+  var text_area= document.querySelector("textarea"); - right
+  var primary_button= document.querySelector(".btn-primary") - right
+var input_button=document.querySelector("#input-btn")-right;
+var input_element=document.getElementsByTagName("#input[name='translator']");-to_check
+
