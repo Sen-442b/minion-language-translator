870637d488594d800159f8e1bd4a0b66365c731a
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-05-03 10:40:18 +0530

    textarea added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/** 
 //input 
 var user_name = prompt("Enter your username");
 
@@ -7,16 +8,29 @@ var welcome_message = `Welcome here  ${user_name}`;
 
 //output
 alert(welcome_message);
-
+**/
 var button_translate = document.querySelector("#btn-translate");
-button_translate.addEventListener("click" , function clickEventHandler(){
-    console.log("clicked")
-});
+var input_textarea = document.querySelector("#txt-input");
+// Declaring Variables Before initializing function is a good practice
+function clickEventHandler(){
+    console.log("clicked");
+    console.log("The input is ",input_textarea.value);
+}
 
-  // exercise question 
+button_translate.addEventListener("click" ,clickEventHandler);
 
+
+
+
+
+
+  /** 
+
+  // EXERCISE QUESTION
+ 
   var text_area= document.querySelector("textarea"); - right
   var primary_button= document.querySelector(".btn-primary") - right
 var input_button=document.querySelector("#input-btn")-right;
 var input_element=document.getElementsByTagName("#input[name='translator']");-to_check
 
+**/
