cf4e60c6c9ba5f7337a2fc6d70168f1aee82636a
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-05-03 18:19:14 +0530

    exercise added

--- main.js
+++ main.js
@@ -12,18 +12,16 @@ alert(welcome_message);
 var button_translate = document.querySelector("#btn-translate");
 var input_textarea = document.querySelector("#txt-input");
 var output_div = document.querySelector("#output");
+
 // Declaring Variables Before initializing function is a good practice
 function clickEventHandler(){
     // console.log("clicked");
     // console.log("The input is ",input_textarea.value);
-output_div.innerText= "translated" + input_textarea.value;
+output_div.innerText= "translated " + input_textarea.value;
 
 }
 
 button_translate.addEventListener("click" ,clickEventHandler);
-
-
-
 
 
 
@@ -35,5 +33,16 @@ button_translate.addEventListener("click" ,clickEventHandler);
   var primary_button= document.querySelector(".btn-primary") - right
 var input_button=document.querySelector("#input-btn")-right;
 var input_element=document.getElementsByTagName("#input[name='translator']");-to_check
+
+
+//home work -1 
+
+let element = document.createElement("div");
+let text = document.createTextNode("ayee yo sup?")
+document.body.appendChild(element);
+//IMP - always add body before appending anything otherwise you will append in DOCUMENT itself
+e
+lement.appendChild(text);
+
 
 **/
