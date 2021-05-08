5f2c22e8f86c4dd0a3d33c9215b0614b1e8bc2bb
Sen-442b <67971019+Sen-442b@users.noreply.github.com>
2021-05-08 11:46:32 +0530

    translation api added

--- main.js
+++ main.js
@@ -12,29 +12,42 @@ alert(welcome_message);
 var button_translate = document.querySelector("#btn-translate");
 var input_textarea = document.querySelector("#txt-input");
 var output_div = document.querySelector("#output");
-var server_URL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+// var encoded_server_URL = encodeURI("https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json")
+var encoded_server_URL = encodeURI("https://api.funtranslations.com/translate/yoda.json")
+
 
 // Declaring Variables Before initializing function is a good practice
 function getTranslationURL(text){
-  return server_URL + "?" + text
+ var request = encoded_server_URL + "?" + "text="+ text
+  return request 
+}
+
+function errorHandler() {
+  alert("server is not working,  try again later")
+
 }
 function clickEventHandler(){
+  
     // console.log("clicked");
     // console.log("The input is ",input_textarea.value);
-//output_div.innerText= "translated " + input_textarea.value;
-var input_text = input_textarea.value; //taking input
+//output_div.innerText= "translated " + input_textarea.value
+var inputText = input_textarea.value //taking input
+
+//calling server for processing 
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+   var translated_text = json.contents.translated 
+  output_div.innerText = translated_text
+})
+.catch(errorHandler)//error handling 
 
 }
 
-//calling server for processing 
-fetch(getTranslationURL(text))
-.then(response => response.json())
-.then(json => console.log(jason));
 button_translate.addEventListener("click" ,clickEventHandler);
 
 
-
-  /** 
+/** 
 
   // EXERCISE QUESTION
  
--- main2.js
+++
@@ -1,5 +0,0 @@
-fetch("https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=work pls")
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-  
